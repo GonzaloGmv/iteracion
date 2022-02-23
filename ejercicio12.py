f2021d98c0015e4c366ef167525da0333a60b3a0
Jose <91785177+jzazooro@users.noreply.github.com>
2022-02-23 18:30:54 +0000

    hecho

--- ejercicio12.py
+++ ejercicio12.py
@@ -1,7 +1,7 @@
 from math imprt sqrt
 def cuadradosperfectos(n): 
-  r = []
-  s = []
+  r=[]
+  s=[]
   for i in range(n+1):
     s.append(i)
   for t in s:
@@ -10,5 +10,5 @@ def cuadradosperfectos(n):
   print("los cuadrados perfectos son: " .format(n))
   for x in r:
     print(x)
-if __name__ == __main__:
+if __name__==__main__:
   n=int(input("introduzca un numero entero: "))
