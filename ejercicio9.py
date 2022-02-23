cad2272570aacec97c28dad404423a03f2f14a49
Gonzalo <91721237+GonzaloGmv@users.noreply.github.com>
2022-02-23 14:35:03 +0000

    9.1

--- ejercicio9.py
+++ ejercicio9.py
@@ -8,11 +8,11 @@ resultado =[]
 letra = input("Escriba la letra que desea:")
 if palabra1[0] == letra:
   resultado.append(palabra1)
-if palabra2[0] == letra:
+elif palabra2[0] == letra:
   resultado.append(palabra2)
-if palabra3[0] == letra:
+elif palabra3[0] == letra:
   resultado.append(palabra3)
-if palabra4[0] == letra:
+elif palabra4[0] == letra:
   resultado.append(palabra4)
 else:
   resultado.append("No hay palabras que empiecen por esa letra")
