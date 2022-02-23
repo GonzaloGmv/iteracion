478a6bd3c446814f5e41cb9600be7ae7cac3f28e
Gonzalo <91721237+GonzaloGmv@users.noreply.github.com>
2022-02-23 17:17:19 +0000

    main

---
+++ ejercicio11.py
@@ -0,0 +1,8 @@
+numero1 = input("Escriba un numero: ")
+numero2 = input("Escriba otro numero: ")
+
+if numero1 == numero2:
+  print("El mcd es ", numero1)
+
+if numero1 > numero2:
+  
--- main.py
+++ main.py
@@ -1,13 +1,10 @@
+from ejercicio9 import ejercicio9
 from ejercicio8 import descomposicion
 
-ejercicio = input("Escriba el numero del ejercicio que quiere que se ejecute": )
-
-if ejercicio == "8":
-  ejercicio8()
-
-from ejercicio9 import ejercicio9
-
-ejercicio = input("Escriba el numero del ejercicio que quiere que se ejecute": )
+ejercicio = input("Escriba el numero del ejercicio que quiere que se ejecute")
 
 if ejercicio == '9':
-  ejercicio9()
+  ejercicio9() 
+  
+elif ejercicio == '8':
+  descomposicion() 
