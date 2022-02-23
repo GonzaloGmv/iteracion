d19b1ba0b691940f10bb6cbbc93be2a0f826f460
Jose <91785177+jzazooro@users.noreply.github.com>
2022-02-23 17:05:54 +0000

    ejercicio8

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+from ejercicio8 import descomposicion
+
+ejercicio = input("Escriba el numero del ejercicio que quiere que se ejecute": )
+
+if ejercicio == "8":
+  ejercicio8()
+
 from ejercicio9 import ejercicio9
 
-ejercicio = input("Escriba el numero del ejercicio que quiere que se ejecute")
+ejercicio = input("Escriba el numero del ejercicio que quiere que se ejecute": )
 
 if ejercicio == '9':
   ejercicio9()
