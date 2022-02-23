dd0eaf1dd2b5a6514f45cf1025bc4a06e806e2f8
Jose <91785177+jzazooro@users.noreply.github.com>
2022-02-23 18:32:52 +0000

    ejercicio 12

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+from ejercicio12 import cuadradosperfectos
 from ejercicio9 import ejercicio9
 from ejercicio8 import descomposicion
 
 ejercicio = input("Escriba el numero del ejercicio que quiere que se ejecute")
+
+if ejercicio == "12":
+  cuadradosperfectos(n)
 
 if ejercicio == '9':
   ejercicio9() 
